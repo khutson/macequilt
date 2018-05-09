d4f65fc513ffd69068a7aeb860e47d255d5d47fa
Kent Hutson <kenthutson@gmail.com>
2018-05-09 00:32:48 -0600

    cleaned up formatting

--- workSpace/wifi.py
+++ workSpace/wifi.py
@@ -1,40 +1,33 @@
 import network
 import time
 
-#add ssid and password here as needed
-ssids = [ ('tsati','hutonetwo'),
-                ('NETGEAR84','yellowtrail736'),
-                ]
-                
-def connect(ip=None,repl=False):
+from wificonfig import ssids
 
+
+def connect(ip=None, repl=False):
     sta_if = network.WLAN(network.STA_IF)
 
     if not sta_if.isconnected():
         sta_if.active(True)
-        
+
         for ssid, pwd in ssids:
-          print('connecting to network...',ssid)
+            print('connecting to network...', ssid)
 
-          sta_if.connect(ssid, pwd)
-          curtime = time.time()
+            sta_if.connect(ssid, pwd)
+            curtime = time.time()
 
-          while not sta_if.isconnected() and (time.time()-curtime)<30 :
-              pass
-          if sta_if.isconnected():
-            break
+            while not sta_if.isconnected() and (time.time() - curtime) < 30:
+                pass
+            if sta_if.isconnected():
+                break
 
     print('network config:', sta_if.ifconfig())
     time.sleep(4)
-    
+
     if repl and sta_if.isconnected():
-      import webrepl
-      webrepl.start()
-
-
+        import webrepl
+        webrepl.start()
 
 
 if __name__ == '__main__':
-  connect()
-
-
+    connect()
---
+++ workSpace/wificonfig.py
@@ -0,0 +1,6 @@
+# add ssid and password here as needed
+# they will be tried in order
+
+ssids = [('tsati', 'hutonetwo'),
+         ('NETGEAR84', 'yellowtrail736'),
+         ]
